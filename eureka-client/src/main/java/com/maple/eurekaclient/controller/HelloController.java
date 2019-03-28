91948508ba003c5a71e74fcdfc092964d04bbac5
blue sky of maple <whb1517@foxmail.com>
2019-03-28 14:20:56 +0800

    Add files via upload
    eureka-client

---
+++ eureka-client/src/main/java/com/maple/eurekaclient/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.maple.eurekaclient.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class HelloController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Resource
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello，host:" + instance.getHost() + ",service_id:" + instance.getServiceId());
+        return "Hello World";
+    }
+}
